af6014b0a0d4e47b42cfc93f6041ef6b2a91755c
Marcosjfp <marcosjfpinheiro@gmail.com>
2025-10-13 15:01:28 +0100

    Added Database Setup to the base code.

--- run.py
+++ run.py
@@ -1,8 +1,12 @@
-#type: ignore
-from app import create_app
+from app import create_app, db
 from flask import render_template
+from app.models import User  # noqa: F401 - Importing the User model to ensure it's registered
 
 app = create_app()
+
+# This block ensures that the database is created when run.py is executed directly.
+with app.app_context():
+    db.create_all() # Creates tables based on models.py
 
 @app.route('/')
 def index():
