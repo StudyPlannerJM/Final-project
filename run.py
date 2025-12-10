6fa23a0e27ad0f19107751299a4547ba70ab90a4
Marcosjfp <marcosjfpinheiro@gmail.com>
2025-12-10 13:38:41 +0000

    Fix PEP 8 violations and code quality issues

--- run.py
+++ run.py
@@ -9,7 +9,6 @@ migrate = Migrate(app, db)
 # This block ensures that the database is created when
 # run.py is executed directly.
 with app.app_context():
-    db.drop_all()  # Drops all tables (for development purposes)
     db.create_all()  # Creates tables based on models.py
     # SIMPLE default admin creator
     admin = User.query.filter_by(email="admin@admin.com").first()
